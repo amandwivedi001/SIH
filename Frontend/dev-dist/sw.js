fd4459b8ffd837d68dd3443371cc0304e254988a
amandwivedi001 <amandwivedi[D262@gmail.com>
2025-09-20 04:54:42 +0530

    change

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jsslpf6mkbg"
+    "revision": "0.mvcq2hbtpdo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- Frontend/src/Components/RoutingMachine.jsx
+++ Frontend/src/Components/RoutingMachine.jsx
@@ -4,7 +4,7 @@ import "leaflet-routing-machine";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
 import { useMap } from "react-leaflet";
 
-const RoutingMachine = ({ origin, destination }) => {
+const RoutingMachine = ({ origin, destination, busStops = [] }) => {
   const map = useMap();
 
   useEffect(() => {
@@ -13,23 +13,21 @@ const RoutingMachine = ({ origin, destination }) => {
     const routingControl = L.Routing.control({
       waypoints: [
         L.latLng(origin[0], origin[1]),
+        ...busStops.map(([lat, lng]) => L.latLng(lat, lng)),
         L.latLng(destination[0], destination[1]),
       ],
       routeWhileDragging: true,
-      lineOptions: {
-        styles: [{ color: "#6FA1EC", weight: 4 }],
-      },
-      show: true, // Set to false to hide the turn-by-turn instructions
-      addWaypoints: false, // Set to false to prevent users from adding more waypoints
+      lineOptions: { styles: [{ color: "#6FA1EC", weight: 4 }] },
+      show: true,
+      addWaypoints: false,
       draggableWaypoints: false,
       fitSelectedRoutes: true,
     }).addTo(map);
 
-    // This is a cleanup function that will run when the component unmounts
     return () => map.removeControl(routingControl);
-  }, [map, origin, destination]);
+  }, [map, origin, destination, busStops]);
 
-  return null; // This component does not render anything itself
+  return null;
 };
 
 export default RoutingMachine;
