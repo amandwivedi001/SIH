04eaefe7a76ee22f8f393732563cf7846fa5c5f8
atreeverma <atreeverma17@gmail.com>
2025-09-20 11:47:15 +0530

    c

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -82,11 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-<<<<<<< HEAD
-    "revision": "0.2ufoj3ssgpg"
-=======
-    "revision": "0.jsslpf6mkbg"
->>>>>>> 793a8c0b6115486f76c203d44daa77f330e99b22
+    "revision": "0.roufm5bfjho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
